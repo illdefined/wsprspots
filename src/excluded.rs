ac8daab6610a3355541d0b77a39547e20c4e3afc
Mikael Voss <mvs@nyantec.com>
2021-02-26 11:56:07 +0100

    Update set of excluded call signs

--- src/excluded.rs
+++ src/excluded.rs
@@ -2,5 +2,7 @@ use phf::{Set, phf_set};
 
 /// Set of excluded call signs
 pub static EXCLUDED: Set<&'static str> = phf_set! {
-	"DL6WAB"  // Not logging WSPR QSOs
+	// Not logging WSPR QSOs
+	"DF2JP",
+	"DL6WAB"
 };
